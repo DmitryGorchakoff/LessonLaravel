e6b5fe3b09ab78cbbca3b2c24812c33ba73d1465
DmitriyGorchakoff <dmitriy.gorchakoff@gmail.com>
2023-09-30 16:48:14 +0300

    Add migration cattegories

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,10 +6,35 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <title>Document</title>
+    <title>Main</title>
+
 </head>
 <body>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{route('post.index')}}">Home</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="">Post</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="">About</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="">Contact</a>
+                    </li>
+                </ul>
+            </div>
 
+        </nav>
+    </div>
+
+</div>
 @yield('content')
 </body>
+
 </html>
